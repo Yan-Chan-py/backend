5043aa8952078327585c17cd13a6ba4b54125e89
CY <fischerbobby865@gmail.com>
2026-06-29 22:10:47 +0200

    fixed db.js

--- db/db.js
+++ db/db.js
@@ -1,20 +1,51 @@
 const { Sequelize } = require('sequelize');
 const config = require('../config');
 
-const sequelize = new Sequelize(
-  config.db.database,
-  config.db.user,
-  config.db.password,
-  {
-    host: config.db.host,
-    port: config.db.port,
+const isProduction = config.nodeEnv === 'production';
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false, // Set to console.log if you want to see SQL queries
+    logging: false,
+    dialectOptions: isProduction
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
     define: {
       timestamps: true,
       underscored: true,
     },
-  }
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    config.db.database,
+    config.db.user,
+    config.db.password,
+    {
+      host: config.db.host,
+      port: config.db.port,
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: isProduction
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+      define: {
+        timestamps: true,
+        underscored: true,
+      },
+    }
+  );
+}
 
 module.exports = sequelize;
